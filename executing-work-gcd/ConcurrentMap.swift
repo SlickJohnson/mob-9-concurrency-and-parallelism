16a1fba7490c86eb358b14b73355ef96e7cce234
SlickJohnson <liwa.johnson@gmail.com>
2018-05-14 17:52:30 -0700

    Implement concurrent map in swift

---
+++ executing-work-gcd/ConcurrentMap.swift
@@ -0,0 +1,10 @@
+extension Array {
+  func parallelMap<T>(_ transform: (Element) -> T)  -> [T] {
+    // Concurrently perform operations of map
+    var resultArray = Array<Element?>.init(repeating: nil, count: self.count)
+    DispatchQueue.concurrentPerform(iterations: self.count) { (i: Int) in
+      let element = self[i]
+      transform(element)
+    }
+  }
+}
