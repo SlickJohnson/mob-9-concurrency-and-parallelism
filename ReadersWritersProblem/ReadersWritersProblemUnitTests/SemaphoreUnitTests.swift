46f4616c6d6556b8c498bdefdb565f8b8c1d0db8
SlickJohnson <liwa.johnson@gmail.com>
2018-05-21 15:40:18 -0700

    Solve readers writers problem using semaphores

--- ReadersWritersProblem/ReadersWritersProblemUnitTests/SemaphoreUnitTests.swift
+++ ReadersWritersProblem/ReadersWritersProblemUnitTests/SemaphoreUnitTests.swift
@@ -11,17 +11,34 @@ import XCTest
 
 class SemaphoreUnitTests: XCTestCase {
 
-    let iterations = 200_000
-    
-    var user: SUser!
-    
-    override func setUp() {
-        super.setUp()
-        user = SUser.current
+  let iterations = 200_000
+
+  var user: SUser!
+
+  override func setUp() {
+    super.setUp()
+    user = SUser.current
+  }
+
+  func testSemaphoreReadersWritersProblem() {
+    // solve with semaphores
+    let expectation = self.expectation(description: "com.makeschool.expectation")
+    let semaphore = DispatchSemaphore(value: 1)
+    let dispatchGroup = DispatchGroup()
+    for _ in 0..<self.iterations {
+      DispatchQueue.global().async(group: dispatchGroup) {
+        print("read: \(self.user.age)")
+        semaphore.wait()
+        self.user.age += 1
+        DispatchQueue.main.async {
+          semaphore.signal()
+        }
+      }
     }
-    
-    func testSemaphoreReadersWritersProblem() {
-        // solve with semaphores
+    dispatchGroup.notify(queue: .main) {
+      expectation.fulfill()
+      XCTAssertEqual(self.iterations, self.user.age)
     }
-    
+    waitForExpectations(timeout: 15, handler: nil)
+  }
 }
